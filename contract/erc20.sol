dca0d55608e74082341f3557d63c928cc047a55e
gubeomlee <29tigerhg@gmail.com>
2023-03-19 11:40:29 +0900

    erc721 buy 함수 회의 필요

---
+++ contract/erc20.sol
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address spender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Transfer(address indexed spender, address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 oldAmount, uint256 amount);
+}
+
+abstract contract OwnerHelper {
+  	address private _owner;
+
+  	event OwnershipTransferred(address indexed preOwner, address indexed nextOwner);
+
+  	modifier onlyOwner {
+			require(msg.sender == _owner, "OwnerHelper: caller is not owner");
+			_;
+  	}
+
+  	constructor() {
+      _owner = msg.sender;
+  	}
+
+    function owner() public view virtual returns (address) {
+      return _owner;
+    }
+
+  	function transferOwnership(address newOwner) onlyOwner public {
+      require(newOwner != _owner);
+      require(newOwner != address(0x0));
+      address preOwner = _owner;
+	    _owner = newOwner;
+	    emit OwnershipTransferred(preOwner, newOwner);
+  	}
+}
+
+library SafeMath {
+  	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+			uint256 c = a * b;
+			assert(a == 0 || c / a == b);
+			return c;
+  	}
+
+  	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+	    uint256 c = a / b;
+			return c;
+  	}
+
+  	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+			assert(b <= a);
+			return a - b;
+  	}
+
+  	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+			uint256 c = a + b;
+			assert(c >= a);
+			return c;
+	}
+}
+
+contract ICToken is ERC20Interface, OwnerHelper {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) public _balances;
+    mapping (address => mapping (address => uint256)) public _allowances;
+
+    uint256 public _totalSupply;
+    string public _name;
+    string public _symbol;
+    uint8 public _decimals;
+
+    constructor() {
+      _name = "8BB";
+      _symbol = "8BB";
+      _decimals = 3;
+      _totalSupply = 100000000;
+      _balances[msg.sender] = _totalSupply;
+    }
+
+    function name() public view returns (string memory) {
+      return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+      return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+      return _decimals;
+    }
+
+    function totalSupply() external view virtual override returns (uint256) {
+      return _totalSupply;
+    }
+
+    function balanceOf(address account) external view virtual override returns (uint256) {
+      return _balances[account];
+    }
+
+    function transfer(address recipient, uint amount) public virtual override returns (bool) {
+      _transfer(msg.sender, recipient, amount);
+      emit Transfer(msg.sender, recipient, amount);
+      return true;
+    }
+
+    function addBalance(address _address, uint256 _amount) external virtual returns(uint256 addressBalance) {
+      _balances[_address] = _balances[_address].add(_amount); 
+      addressBalance = _balances[_address]; 
+    }
+
+    function subBalance(address _address, uint256 _amount) external virtual returns(uint256 addressBalance) {
+      _balances[_address] = _balances[_address].sub(_amount); 
+      addressBalance = _balances[_address]; 
+    }
+
+    function allowance(address owner, address spender) external view override returns (uint256) {
+      return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint amount) external virtual override returns (bool) {
+      uint256 currentAllowance = _allowances[msg.sender][spender];
+      require(_balances[msg.sender] >= amount,"ERC20: The amount to be transferred exceeds the amount of tokens held by the owner.");
+      _approve(msg.sender, spender, currentAllowance, amount);
+      return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external virtual override returns (bool) {
+      _transfer(sender, recipient, amount);
+      emit Transfer(msg.sender, sender, recipient, amount);
+      uint256 currentAllowance = _allowances[sender][msg.sender];
+      require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+      _approve(sender, msg.sender, currentAllowance, currentAllowance - amount);
+      return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+      require(sender != address(0), "ERC20: transfer from the zero address");
+      require(recipient != address(0), "ERC20: transfer to the zero address");
+      uint256 senderBalance = _balances[sender];
+      require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+      _balances[sender] = senderBalance.sub(amount);
+      _balances[recipient] = _balances[recipient].add(amount);
+    }
+
+    function _approve(address owner, address spender, uint256 currentAmount, uint256 amount) internal virtual {
+      require(owner != address(0), "ERC20: approve from the zero address");
+      require(spender != address(0), "ERC20: approve to the zero address");
+      _allowances[owner][spender] = amount;
+      emit Approval(owner, spender, currentAmount, amount);
+    }
+
+    event NewPosting(string title, string author, uint256 postingTime);
+    event EditPosting(uint256 postingTime); 
+    event AddComment(uint256 commentTime); 
+    event EditComment(uint256 commentTime);
+
+    uint256 public airDropTokenPostAmount = 50;
+    uint256 public airDropTokenCommentAmount = 10; 
+    uint256 postId = 0; //postId는 1부터 시작한다. 
+
+    struct NewPost {
+        string title; // 새 포스팅의 제목.
+        string author; // 표시되는 저자명. 
+        string body; // 글의 본문. 
+        uint256 postingTime; // 글을 작성한 시간.
+        uint256 commentCount; // 0으로 시작해서 댓글을 하나 쓸때마다 1씩 더한다. 
+        bool edit; // 글을 수정하기 전에는 false. 
+    }
+    struct Comment {
+        string comment; 
+        uint256 commentTime; 
+    }
+
+    mapping (uint256 => NewPost) postIdToNewPost; 
+    mapping (uint256 => address) postIdToAddress;
+    mapping (uint256 => mapping(uint256 => Comment)) commentIdToComment; 
+    mapping (uint256 => mapping(uint256 => address)) commentIdToAddress;
+    
+
+    function generateNewPost (string memory _title, string memory _author, string memory _body) public returns(uint256) {
+        postId = postId.add(1); 
+        postIdToNewPost[postId] = NewPost(_title, _author, _body, block.timestamp, 0, false);
+        postIdToAddress[postId] = msg.sender; 
+        address _recipient = msg.sender; 
+        _airDropTokenToGenerateNewPost(_recipient);
+        emit NewPosting(_title, _author, block.timestamp);
+        return postId.sub(1); // postId에 1을 더했기 때문에 출력하는 값은 1을 뺀 값을 출력한다.  
+    }
+
+    function _airDropTokenToGenerateNewPost (address _recipient) private {
+        address _owner = owner();
+        _transfer(_owner, _recipient, airDropTokenPostAmount);
+    }
+
+    function editPost (string memory _body, uint256 _postId) public {
+        require(msg.sender == postIdToAddress[_postId]);
+        postIdToNewPost[_postId].body = _body; 
+        postIdToNewPost[_postId].postingTime = block.timestamp;
+        postIdToNewPost[_postId].edit = true;
+        emit EditPosting(block.timestamp);
+    }
+
+    function addComment (string memory _comment, uint256 _postId) public returns(uint256 commentId) {
+        commentId = postIdToNewPost[_postId].commentCount.add(1);
+        commentIdToComment[_postId][commentId].comment = _comment; 
+        commentIdToComment[_postId][commentId].commentTime = block.timestamp; 
+        commentIdToAddress[_postId][commentId] = msg.sender;
+        postIdToNewPost[_postId].commentCount.add(1);  
+        address _recipient = msg.sender; 
+        _airDropTokenToComment(_recipient);
+        emit AddComment(block.timestamp);
+    }
+
+    function _airDropTokenToComment (address _recipient) private {
+        address _owner = owner();
+        _transfer(_owner, _recipient, airDropTokenCommentAmount);
+    } 
+
+    function editComment (string memory _comment, uint256 _postId, uint256 _commentId) public {
+        require(msg.sender == commentIdToAddress[_postId][_commentId]); 
+        commentIdToComment[_postId][_commentId].comment = _comment; 
+        commentIdToComment[_postId][_commentId].commentTime = block.timestamp; 
+        emit EditComment(block.timestamp);
+    }
+
+    function showPostById(uint256 _postId) public view returns(NewPost memory post) {
+        post = postIdToNewPost[_postId]; 
+    }
+
+    function showCommentById(uint256 _postId, uint256 _commentId) public view returns(Comment memory comment) {
+        comment = commentIdToComment[_postId][_commentId]; 
+    }
+
+    function totalPost() public view returns(uint256) {
+        return postId; 
+    }
+}
---
+++ contract/erc721.sol
@@ -0,0 +1,109 @@
+//Contract based on [<https://docs.openzeppelin.com/contracts/3.x/erc721>](<https://docs.openzeppelin.com/contracts/3.x/erc721>)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "./testERC20.sol";
+interface TestERC20Interface {
+    function balanceOf(address account) external view returns (uint256);
+    function addBalance(address _address, uint256 _amount) external returns(uint256 addressBalance);
+    function subBalance(address _address, uint256 _amount) external returns(uint256 addressBalance);
+}
+
+contract MyNFTs is ERC721URIStorage, Ownable {
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("8BB_NFT", "8Bb_NFT") {}
+
+    //interface 연결하는 함수.
+    //erc20.sol ICToken 배포 후 erc721.sol MyNFTs 배포 
+    //erc20.sol ICToken의 컨트랙트 주소를 함수에 입력하면 두 컨트랙트가 연결됩니다. 
+    TestERC20Interface erc20Contaract; 
+    function setTestERC20InterfaceAddress(address _address) external onlyOwner {
+        erc20Contaract = TestERC20Interface(_address); 
+    }
+
+    //잔고 확인. 함수 실행한 주소의 ERC20토큰 잔고를 보여 준다. 
+    function balanceERC20() public view returns(uint256 balance) {
+        balance = erc20Contaract.balanceOf(msg.sender);
+    }
+
+    //erc20토큰 전송 함수. mapping 직접 활용.
+    //interface로 transfer 함수 가져오면 실행되지 않는 오류가 발생 
+    //erc20.sol에 _balances mapping을 관리하는 함수를 만들고 해당 함수를 가져와 함수 만든 것. 
+    function erc20transfer(address _recipient, uint256 _amount) private returns(uint256 senderBalance, uint256 recipientBalance){
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(erc20Contaract.balanceOf(msg.sender) >= _amount, "ERC20: transfer amount exceeds balance");
+        senderBalance = erc20Contaract.subBalance(msg.sender, _amount);
+        recipientBalance = erc20Contaract.addBalance(_recipient, _amount); 
+    }
+
+    // 민팅 비용 설정, 편의상 민팅 비용은 15로 설정한다. 
+    uint256 mintingFee = 15; 
+    function setMintingFee(uint256 _mintingFee) external onlyOwner returns(uint256) {
+        mintingFee = _mintingFee; 
+        return mintingFee;
+    } 
+
+    // nft 판매 정보가 담긴 구조체 
+    struct NFTInfo { 
+        uint256 price;
+        bool isForSale; // 판매 대상이 아니라면 false, 판매 중이라면 ture
+    }
+    mapping(uint256 => NFTInfo) tokenIdToNFTInfo;
+
+    // 누구나 민팅할 수 있도록 onlyOwner 제거 
+    function mintNFT(string memory tokenURI)
+        public 
+        returns (uint256)
+    {
+        // NFT 민팅 할 때 mintingFee에 해당하는 금액을 contract owner에게 전송해야 한다.
+        // _owner가 민팅 할 때는 비용을 지불하지 않거나, _owner는 민팅할 수 없게 설정해야 할 것 같음. 일단은 전자로 작성함.   
+        address _owner = owner();
+        if(msg.sender != _owner){
+            erc20transfer(_owner, mintingFee);
+        }
+
+        _tokenIds.increment();
+
+        address recipient = msg.sender; 
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        // 민팅할 때 판매 정보 구조체 mapping
+        tokenIdToNFTInfo[newItemId] = ForSale(0, false);
+
+        return newItemId;
+    } 
+
+    // 판매정보 구조체 수정 
+    function sell (uint256 _tokenId, uint256 _price) public onlyOwner {
+        tokenIdToNFTInfo[_tokenId].price = _price; 
+        tokenIdToNFTInfo[_tokenId].isForSale = true; 
+    }
+
+    function tokenIdToPrice (uint256 _tokenId) public returns(uint256, string memory) {
+        if(tokenIdToNFTInfo[_tokenId].isForSale){
+            return tokenIdToNFTInfo[_tokenId].price; 
+        } else {
+            return "This item is not for sale."; 
+        }
+    }
+  
+    // 구매자가 nft를 이동시킬 수 없음. 따라서 구매함수 만으로 nft이동시키는 것 불가.
+    // 구매 메커니즘 회의에서 결정하기. event 메커니즘 활용할 경우 백에서 처리해줘야 함.     
+    function buy (uint256 _saleId) public {
+        require(tokenIdToNFTInfo[_tokenId].isForSale == false, "This item is not for sale.");
+        require(erc20Contaract.balanceOf(msg.sender) >= tokenIdToNFTInfo[_tokenId].price, "price exceeds balance");  
+        address _NFTOwner = _ownerOf(forsale[_saleId].tokenId); 
+        // 미완성 코드
+    }
+}
