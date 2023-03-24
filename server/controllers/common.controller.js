c7c1210482420483d138479422007390b47ae835
soma <macaronieyes@gmail.com>
2023-03-24 09:55:39 +0900

    fix: user amout 업데이트 결과값 수정

--- server/controllers/common.controller.js
+++ server/controllers/common.controller.js
@@ -4,13 +4,24 @@ const { getBalance } = require('../services/web3.erc20.services');
 
 module.exports = {
   user_amountUpdate: async (id, address, tx) => {
-    let eth_amount = await getEtherAmount(address);
-    eth_amount = await web3.utils.fromWei(eth_amount, 'ether').substring(0, 9);
-    const token_amount = await getBalance(address);
-    const updateValue = {id: id};
-    if(eth_amount) updateValue.eth_amount = eth_amount;
-    if(token_amount) updateValue.token_amount = token_amount;
+    try {
+      let eth_amount = await getEtherAmount(address);
+      eth_amount = await web3.utils.fromWei(eth_amount, 'ether').substring(0, 9);
+      const token_amount = await getBalance(address);
+      const updateValue = {id: id};
+      if(eth_amount) updateValue.eth_amount = eth_amount;
+      if(token_amount) updateValue.token_amount = token_amount;
 
-    return await user.user_update(updateValue, tx);
+      const updateResult = await user.user_update(updateValue, tx);
+      if(!updateResult) return null;
+      return {
+        updateResult: updateResult,
+        eth_amount: eth_amount,
+        token_amount: token_amount
+      }
+    } catch (e){
+      console.log(e);
+      return null;
+    }    
   }
 }
